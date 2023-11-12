d541c5bcb24b44b9d08e10c3bf280a82728b351e
Kamil Wrobel <wrobelkam@gmail.com>
2023-11-12 08:50:20 +0100

    first commit

---
+++ src/main/java/com/crud/tasks/domain/Taks.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@AllArgsConstructor
+@Getter
+public class Taks {
+    private long id;
+    private String title;
+    private String content;
+}
