a837bdd57cd2f0fc2d90dbffc0303a9286470c48
Kamil Wrobel <wrobelkam@gmail.com>
2023-11-13 14:01:35 +0100

    19.2 wyszukiwanie pojedynczego taska

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.crud.tasks.domain.Task;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task,Long> {
+    List<Task> findAll();
+    @Override
+    Optional<Task> findById(Long id);
+}
