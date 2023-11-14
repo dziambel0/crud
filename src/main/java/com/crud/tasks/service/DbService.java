f07c58c057a7a7747daf89baa697964cf74b8d7d
Kamil Wrobel <wrobelkam@gmail.com>
2023-11-14 13:53:30 +0100

    19.3 DELETE

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import com.crud.tasks.domain.Task;
@@ -17,7 +18,14 @@ public class DbService {
     public List<Task> getAllTasks(){
         return repository.findAll();
     }
-    public Optional<Task> getTask(final Long id) {
-        return repository.findById(id);
+
+    public Task getTask(final Long id) throws TaskNotFoundException {
+        return repository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+    public void deleteTask(final Long id){
+        repository.deleteById(id);
     }
 }
