d3f5a5802f20b9c0d18151d0c0b79e729af9179c
Kamil Wrobel <wrobelkam@gmail.com>
2023-12-03 14:42:49 +0100

    22.3 Rozbudowa klienta

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,12 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,14 @@ public class TrelloController {
                 .filter(b->b.getName().contains("Kodilla"))
                 .forEach(trelloBoardDto -> {
             System.out.println(trelloBoardDto.getId()+" "+trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloLis -> {
+                System.out.println(trelloLis.getId()+" - "+trelloLis.getName()+" - "+trelloLis.isClosed());
+            });
         });
+    }
+    @PostMapping("cards")
+    public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return trelloClient.createNewCard(trelloCardDto);
     }
 }
